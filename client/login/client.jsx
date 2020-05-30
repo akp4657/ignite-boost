458290e706b36a3c46f17a63607f0064d22dcb15
Anthony Pichardo <anthonykpichardo@gmail.com>
2020-05-30 14:22:07 -0500

    Small changed

--- client/login/client.jsx
+++ client/login/client.jsx
@@ -210,6 +210,7 @@ const VideoList = function(props) {
                         <td>
                             <a href={video.link} className="icons-sm yt-ic" target="_blank"><i className="fab fa-youtube fa-2x"> </i></a>
                         </td>
+                        <td>{versionImg}</td>
                     </tr>
                 </tbody>
 
