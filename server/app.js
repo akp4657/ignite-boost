27e2db193845d1b31a2baa954d8e44aefa7be770
GaryButternubs <GaryButternubs123@gmail.com>
2025-09-20 16:57:16 -0400

    Added CSP through helmet.

--- server/app.js
+++ server/app.js
@@ -9,11 +9,11 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const helmet = require('helmet');
+const crypto = require('crypto');
+const fs = require('fs');
 
 // Pull in our routes
 const router = require('./router');
-
-const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 let mongoURL;
 if(process.env.NODE_ENV == 'production') mongoURL = process.env.PROD_MONGO
@@ -28,22 +28,20 @@ mongoose.connect(mongoURL).catch((err) => {
 const app = express();
 
 app.disable('x-powered-by');
-// app.use(helmet({
-//   contentSecurityPolicy: {
-//     directives: {
-//       'script-src': [
-//         "'self'", 
-//         'https://youtube.com', 
-//         'https://twitch.tv', 
-//         'https://nicovideo.jp',
-//         'https://bilibili.com'
-//       ],
-//     },
-//   },
-// }));
-// Serve static files from the Angular dist directory
-app.use('/assets', express.static(path.resolve(`${__dirname}/../dist/browser/assets`)));
-app.use(express.static(path.resolve(`${__dirname}/../dist/browser`)));
+//app.use(helmet());
+app.use((req, res, next) => {
+  res.locals.cspNonce = crypto.randomBytes(32).toString("hex");
+  next();
+});
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        'script-src': ["'self'", (req, res) => `'nonce-${res.locals.cspNonce}'`],
+      },
+    },
+  }),
+);
 
 app.use(compression());
 
@@ -71,10 +69,20 @@ app.use((err, req, res, next) => {
 
 router(app);
 
-app.get('/{*splat}', (req, res) => {
-  res.sendFile(path.resolve(`${__dirname}/../dist/browser/index.html`));
+app.use(express.static(path.resolve(`${__dirname}/../dist/browser`), {
+  index: false, // Don't serve index.html statically
+}));
+app.get(`/{*splat}`, (req, res) => {
+  const indexPath = path.join(__dirname, '/../dist/browser', 'index.html');
+  fs.readFile(indexPath, 'utf8', (err, data) => {
+    if (err) return res.status(500).send("Error loading index.html");
+
+    const indexWithNonce = data.replaceAll('randomNonceGoesHere', res.locals.cspNonce);
+    res.send(indexWithNonce);
+  });
 });
 
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
 app.listen(port, (err) => {
   if (err) {
     throw err;
