c4bb75d45ec0bae91375f9c595b75193f6039b7a
Spine <anthonykpichardo@gmail.com>
2025-10-01 10:29:02 -0400

    Debug logging for Heroku

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -15,18 +15,32 @@ const requiresLogout = (req, res, next) => {
 };
 
 const requiresSecure = (req, res, next) => {
+    // Debug logging for Heroku
+    console.log('=== HTTPS Middleware Debug ===');
+    console.log('URL:', req.url);
+    console.log('Method:', req.method);
+    console.log('req.secure:', req.secure);
+    console.log('x-forwarded-proto:', req.headers['x-forwarded-proto']);
+    console.log('x-forwarded-ssl:', req.headers['x-forwarded-ssl']);
+    console.log('host:', req.hostname);
+    console.log('NODE_ENV:', process.env.NODE_ENV);
+  
   // Need to instead check if the original request was HTTPS since we're on Heroku
   const isHttps = req.secure || 
                   req.headers['x-forwarded-proto'] === 'https' ||
                   (req.headers['x-forwarded-ssl'] === 'on');
   
+  console.log('isHttps:', isHttps);
   if (!isHttps) {
+    console.log('Redirecting to HTTPS');
     return res.redirect(`https://${req.hostname}${req.url}`);
   }
+  
   return next();
 };
 
 const bypassSecure = (req, res, next) => {
+  console.log('Bypassing HTTPS');
   next();
 };
 
