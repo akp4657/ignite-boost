fa0779aa827bb1b09c626e34d642f34b93d04886
Spine <anthonykpichardo@gmail.com>
2025-10-01 10:46:51 -0400

    Bypass extra security as it turns out, it is redundant for Heroku

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -15,35 +15,19 @@ const requiresLogout = (req, res, next) => {
 };
 
 const requiresSecure = (req, res, next) => {
-    // Debug logging for Heroku
-    console.log('=== HTTPS Middleware Debug ===');
-    console.log('URL:', req.url);
-    console.log('Method:', req.method);
-    console.log('req.secure:', req.secure);
-    console.log('x-forwarded-proto:', req.headers['x-forwarded-proto']);
-    console.log('x-forwarded-ssl:', req.headers['x-forwarded-ssl']);
-    console.log('host:', req.hostname);
-    console.log('NODE_ENV:', process.env.NODE_ENV);
-  
-  // Need to instead check if the original request was HTTPS since we're on Heroku
-  const isHttps = req.secure || 
-                  req.headers['x-forwarded-proto'] === 'https' ||
-                  (req.headers['x-forwarded-ssl'] === 'on');
-  
-  console.log('isHttps:', isHttps);
-  if (!isHttps) {
-    console.log('Redirecting to HTTPS');
+  if (req.headers['x-forwarded-proto'] !== 'https') {
     return res.redirect(`https://${req.hostname}${req.url}`);
   }
-  
   return next();
 };
 
 const bypassSecure = (req, res, next) => {
-  console.log('Bypassing HTTPS');
   next();
 };
 
 module.exports.requiresLogin = requiresLogin;
 module.exports.requiresLogout = requiresLogout;
-module.exports.requiresSecure = (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'qa') ? requiresSecure : bypassSecure; 
+
+// Heroku enforces HTTPS, and unfortunately the headers aren't set all the time from Heroku
+// So instead, we just completely bypass this security check
+module.exports.requiresSecure = bypassSecure; 
