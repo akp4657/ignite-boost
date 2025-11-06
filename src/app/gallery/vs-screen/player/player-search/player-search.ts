27078ea5ddf5d6cb37ec7d03a1f7817198ac8e39
GaryButternubs <GaryButternubs123@gmail.com>
2025-11-06 10:28:22 -0500

    Updated player-search component to show p1 name during URL search.

--- src/app/gallery/vs-screen/player/player-search/player-search.ts
+++ src/app/gallery/vs-screen/player/player-search/player-search.ts
@@ -1,4 +1,5 @@
-import { Component, input, output, signal } from '@angular/core';
+import { Component, OnInit, inject, input, output, signal } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { NgClass } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field'
@@ -10,7 +11,9 @@ import { MatInputModule } from '@angular/material/input';
   templateUrl: './player-search.html',
   styleUrl: './player-search.scss'
 })
-export class PlayerSearch {
+export class PlayerSearch implements OnInit {
+  route = inject(ActivatedRoute);
+
   isP1 = input<boolean>(true); // Affects skew direction
   isFull = input<boolean>(true);
   updatePlayerQuery = output<string>();
@@ -20,5 +23,13 @@ export class PlayerSearch {
   updatePlayerInput(player: string) {
     this.playerInput.set(player);
     this.updatePlayerQuery.emit(player);
+  }
+
+  ngOnInit(): void {
+    if (this.isP1()) {
+      this.route.params.subscribe((params) => {
+        this.playerInput.set(params['name']);
+      });
+    }
   }
 }
